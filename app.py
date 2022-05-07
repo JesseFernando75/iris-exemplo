ddb25c8343914ffebb1fb01f45a26c8dc89498b7
Jessé Fernando <62728622+JesseFernando75@users.noreply.github.com>
2022-05-06 21:45:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ predicao = dtc.predict(df)
 st.write(f"A classe dessa flor é: **{predicao[0]}")
 
 predicao = dtc.predict_proba(df)
-predicao = pdf.DataFrame(predicao)
+predicao = pd.DataFrame(predicao)
 
 predicao.rename({
   0 : "Iris-setosa",
